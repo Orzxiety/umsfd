9810f41549cb472587003fe4732dd55118f63092
Orzxiety <83078982+Orzxiety@users.noreply.github.com>
2023-02-12 21:09:48 +0500

    Add files via upload

---
+++ decoder.py
@@ -0,0 +1,18 @@
+from base64 import b64decode
+from gzip import decompress
+
+filename = "YOUR_FILE_NAME"
+
+print("Decoding...", end=" ")
+
+file = open(filename, "rb")
+file = file.read()
+file = b64decode(file)
+
+data = decompress(file)
+mystring = str(data, "utf-8")
+
+output = open("output.txt", "w")
+output.write(mystring)
+
+print("done (output.txt)")
